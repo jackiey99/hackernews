90bbbd95486fd0e6a0dad6af2182db0834868816
Liangyue Li <jackiey99@gmail.com>
2015-07-28 14:57:18 -0700

    CrawlSpider to follow links

--- hackerbot/hackerbot/spiders/hacker_spider.py
+++ hackerbot/hackerbot/spiders/hacker_spider.py
@@ -1,13 +1,26 @@
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 from hackerbot.items import HackerbotItem
 
-class HackerSpider(scrapy.Spider):
+class HackerSpider(CrawlSpider):
 	name = "hacker"
 	start_urls = [
 		"https://news.ycombinator.com/",
 	]
 
-	def parse(self, response):
+	rules = (
+		Rule(LinkExtractor(allow = r'news\?p=\d*'), callback='parse_item', follow=True),
+	)
+
+	def parse_start_url(self, response):
+		for sel in response.xpath('//tr[@class="athing"]/td[3]'):
+			item = HackerbotItem()
+			item['title'] = sel.xpath('a/text()').extract()[0]
+			item['link'] = sel.xpath('a/@href').extract()[0]
+			yield item
+
+	def parse_item(self, response):
 		for sel in response.xpath('//tr[@class="athing"]/td[3]'):
 			item = HackerbotItem()
 			item['title'] = sel.xpath('a/text()').extract()[0]
